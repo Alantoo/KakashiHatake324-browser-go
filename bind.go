2f2cfe4faecb1ee3ad07d048627c4b379fac997e
rafaeltorres324@gmail.com <rafaeltorres324@gmail.com>
2024-05-05 22:29:14 -0400

    Update bind.go

--- bind.go
+++ bind.go
@@ -1,10 +1,14 @@
 package browsergo
 
 import (
+	"bufio"
 	"context"
 	_ "embed"
+	"fmt"
+	"io"
 	"log"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/KakashiHatake324/browser-go/browser/exec"
@@ -17,14 +21,45 @@ func (c *ClientInit) launchServer() error {
 	if err != nil {
 		return err
 	}
+	reader, writer := io.Pipe()
+	scanner := bufio.NewScanner(reader)
 	cmd := exe.CommandContext(context.Background(), strconv.Itoa(c.port), strconv.FormatBool(c.verbose))
 	c.closeExe = exe.Close
+	cmd.Stdout = writer
 	if err := cmd.Start(); err != nil {
 		return err
 	}
-	if c.verbose {
-		log.Println("[LAUNCH SERVER] server launched successfully")
+
+	var loaded bool
+	go func() {
+		for scanner.Scan() {
+			if c.verbose {
+				fmt.Println(scanner.Text())
+			}
+			if !loaded {
+				if strings.Contains(scanner.Text(), "ERROR") {
+					if c.verbose {
+						log.Println("[LAUNCH SERVER] error launching server:", err)
+					}
+					err = errServerConnection
+					loaded = true
+				} else if strings.Contains(scanner.Text(), "server is running") {
+					if c.verbose {
+						log.Println("[LAUNCH SERVER] server launched successfully")
+					}
+					loaded = true
+				}
+			}
+		}
+	}()
+
+	for !loaded {
+		select {
+		case <-c.CTX.Done():
+			return nil
+		default:
+			time.Sleep(1 * time.Second)
+		}
 	}
-	time.Sleep(5 * time.Second)
 	return err
 }
