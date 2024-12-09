2b6461f9e464fabe6245ba4b1463dd446bf6de0f
rafaeltorres324@gmail.com <rafaeltorres324@gmail.com>
2024-12-09 14:55:50 -0500

    update

--- shape_test.go
+++ shape_test.go
@@ -8,7 +8,7 @@ import (
 	browsergo "github.com/KakashiHatake324/browser-go"
 )
 
-const shapeProxy = "p6.mushroomproxy.com:8000:1F7Aq7B16-mushroom-b70bf8ba79!g-us!f-hddv!sid-yLVblzPnMzY:1d7k3jfj3kiuf3f"
+const shapeProxy = "31.128.120.186:63690:yvyyhlia:s4XI854jtl"
 
 func TestShape(t *testing.T) {
 	service, err := browsergo.InitBrowser("browser-go-test", true, "")
@@ -23,10 +23,10 @@ func TestShape(t *testing.T) {
 	defer instance.Close()
 
 	browserOpts := &browsergo.BrowserOpts{
-		StartUrl:     "",
-		Proxy:        shapeProxy,
-		Args:         []browsergo.FlagType{},
-		Headless:     true,
+		StartUrl: "",
+		Proxy:    shapeProxy,
+		Args:     []browsergo.FlagType{},
+		//Headless:     true,
 		OpenDevtools: false,
 		WaitLoad:     false,
 	}
@@ -39,9 +39,9 @@ func TestShape(t *testing.T) {
 		BrowserService: instance,
 		Context:        context.TODO(),
 		Deadline:       45,
-		ScriptUrl:      browsergo.NewBalance,
-		UserAgent:      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36",
-		RequestUrl:     "https://www.newbalance.com/on/demandware.static/Sites-NBUS-Site/en_US/CheckoutServices-PlaceOrder?termsconditions=undefined&DFReferenceId=",
+		ScriptUrl:      browsergo.Target,
+		UserAgent:      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
+		RequestUrl:     "https://gsp.target.com/gsp/authentications/v1/credential_validations?client_id=ecom-web-1.0.0",
 		RequestMethod:  "POST",
 		ProxyString:    proxy,
 	}
